695ef338e52d5e22a094d46640c48c7e05bdf2aa
Revannath-Khodade <revannathkhodade01@gmail.com>
2026-02-24 11:52:27 +0530

    Initial commit

---
+++ src/test/java/net/javaguides/sms/StudentManagementSystemApplicationTests.java
@@ -0,0 +1,11 @@
+package test.java.net.javaguides.sms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+@SpringBootTest
+public class StudentManagementSystemApplicationTests {
+    @Test
+    void contextLoads() {
+
+    }
+}
